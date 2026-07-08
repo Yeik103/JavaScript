aaaa6c49648d0474c9a1c361f011e2360aa19838
Yeik103 <espinozayeisy803@gmail.com>
2026-07-08 17:51:00 -0300

    08/07

---
+++ JS/ex03.js
@@ -0,0 +1,10 @@
+//Criando variáveis
+var animal = "Joaninha"
+var carac  = "?"
+var idade  = 12
+var altura = 1.80
+
+console.log(animal)
+console.log(carac)
+console.log(idade)
+console.log(altura)
---
+++ JS/ex05.js
@@ -0,0 +1,11 @@
+//Operadores Unários
+var numero = 2 
+console.log(numero)
+numero ++// ++ chamado de incremento
+console.log(numero)
+
+
+var n = 5
+console.log(n)
+n -- // chamado de decremento
+console.log(n)
