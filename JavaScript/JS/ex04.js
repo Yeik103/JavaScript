e736c1a103e4a13117b5914ef84e6f057c743c73
Yeik103 <espinozayeisy803@gmail.com>
2026-07-09 17:48:27 -0300

    09/07

---
+++ JavaScript/JS/ex04.js
@@ -0,0 +1,17 @@
+var numero1 = 12
+var numero2 = 7
+
+var soma = numero1 + numero2
+console.log("A soma é:", soma) //exemplo de concatenar
+
+var sub = numero1 - numero2
+console.log("O resultado de:", numero1, " - ", numero2, "é:", sub)
+
+var veces = numero1 * numero2
+console.log(veces, "É a multiplicação de:", numero1, "X", numero2)
+
+var div = numero1 / numero2
+console.log("O resultado de:", numero1, " / ", numero2, "é:",div)
+
+var resto = numero1 % numero2
+console.log("O resto da div de:", numero1, " % ", numero2, "é:",resto)
---
+++ JavaScript/JS/ex06.js
@@ -0,0 +1,13 @@
+var a = 10
+var b = "10"
+//console.log(a == b)
+//console.log(b === a)
+
+var c = 10
+var d =  2
+//console.log(c != d)
+//console.log(a != b)
+//console.log(c > d)
+//console.log(c >= a)
+//console.log(c <= a)
+console.log(d < c)
